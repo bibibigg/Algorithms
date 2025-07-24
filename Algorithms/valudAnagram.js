5a611e0115143ef43fc29fd3ca7aef41f3a63374
bibibigg <aerl0706@naver.com>
2025-07-25 01:40:06 +0900

    validAnagram

---
+++ Algorithms/valudAnagram.js
@@ -0,0 +1,46 @@
+/*
+빈도수 세기- validAnagram
+두 개의 문자열이 주어졌을 때, 두 번째 문자열이 첫 번째 문자열의 애너그램인지 확인하는 함수를 작성합니다. 애너그램은 다른 글자의 글자를 재배열하여 형성된 단어, 구 또는 이름입니다. (예시: cinema -> iceman)
+
+예시:
+
+validAnagram('', '') // true
+validAnagram('aaz', 'zza') // false
+validAnagram('anagram', 'nagaram') // true
+validAnagram("rat","car") // false) // false
+validAnagram('awesome', 'awesom') // false
+validAnagram('amanaplanacanalpanama', 'acanalmanplanpamana') // false
+validAnagram('qwerty', 'qeywrt') // true
+validAnagram('texttwisttime', 'timetwisttext') // true
+안내: 문자열에 소문자만 포함되어 있다고 가정해도 됩니다.
+
+Time Complexity - O(n)
+*/
+
+function validAnagram(str1, str2) {
+  if (str1.length !== str2.length) {
+    return false;
+  }
+
+  let frequancyCounter1 = {};
+  let frequancyCounter2 = {};
+
+  for (let val of str1) {
+    frequancyCounter1[val] = (frequancyCounter1[val] || 0) + 1;
+  }
+  for (let val of str2) {
+    frequancyCounter2[val] = (frequancyCounter2[val] || 0) + 1;
+  }
+
+  // 키에 해당하는 문자가 str2에 없으면 false
+  for (let key in frequancyCounter1) {
+    if (!(key in frequancyCounter2)) {
+      return false;
+    }
+    //키에 해당하는 문자가 str2에 있지만 개수가 다르면 false
+    if (frequancyCounter2[key] !== frequancyCounter1[key]) {
+      return false;
+    }
+  }
+  return true;
+}
