005d32944d14d6ee3ccff62f1d054267a80b562a
bibibigg <aerl0706@naver.com>
2025-08-18 17:49:27 +0900

    다중포인터 isSubsequence

---
+++ isSubsequence.js
@@ -0,0 +1,15 @@
+function isSubsequence(str1, str2) {
+  let str1Index = 0;
+  let str2Index = 0;
+
+  while (str2Index < str2.length) {
+    if (str1[str1Index] === str2[str2Index]) {
+      str1Index++;
+    }
+    if (str1Index === str1.length) {
+      return true;
+    }
+    str2Index++;
+  }
+  return false;
+}
